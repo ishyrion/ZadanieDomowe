ca55523cb7503ef9c1f46dd94393cd7ec40e6a0c
ishyrion <44479888+ishyrion@users.noreply.github.com>
2019-02-03 09:46:18 +0100

    Add files via upload

---
+++ TimeOverride/TimeOverride/Time.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeOverride
+{
+    struct Time : IEquatable<Time>, IComparable<Time>
+    {
+        private readonly byte hours;
+        private readonly byte minutes;
+        private readonly byte seconds;
+        private readonly ushort miliseconds;
+
+        public byte Hours => hours;
+        public byte Minutes => minutes;
+        public byte Seconds => seconds;
+        public ushort Miliseconds => miliseconds;
+
+        public Time(byte hours, byte minutes = 0, byte seconds = 0, ushort miliseconds = 0)
+        {
+            this.hours = hours;
+            this.minutes = minutes;
+            this.seconds = seconds;
+            this.miliseconds = 0;
+
+            if (hours >= 24 || minutes >= 60 || seconds >= 60 || miliseconds >= 1000)
+                throw new ArgumentOutOfRangeException();
+        }
+
+        public Time(Time other)
+        {
+            hours = other.Hours;
+            minutes = other.Minutes;
+            seconds = other.Seconds;
+            miliseconds = other.Miliseconds;
+        }
+
+
+        public Time(string s)
+        {
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+            miliseconds = 0;
+
+            if (s.Length == 8 || s.Length == 12)
+                throw new ArgumentException();
+
+            if (Byte.TryParse(s.Substring(0, 2), out hours) == false)
+                throw new ArgumentException();
+            if (hours >= 24)
+                throw new ArgumentOutOfRangeException();
+
+            if (Byte.TryParse(s.Substring(3, 5), out minutes) == false)
+                throw new ArgumentException();
+            if (minutes >= 60)
+                throw new ArgumentOutOfRangeException();
+
+            if (Byte.TryParse(s.Substring(6, 8), out seconds) == false)
+                throw new ArgumentException();
+            if (seconds >= 60)
+                throw new ArgumentOutOfRangeException();
+
+            if (s.Length == 12 && UInt16.TryParse(s.Substring(9, 12), out miliseconds) == false)
+                throw new ArgumentException();
+        }
+
+        public override string ToString()
+        {
+            return $"{hours / 10}{hours % 10}:{minutes / 10}{minutes % 10}:{seconds / 10}{seconds % 10}";
+        }
+
+        public bool Equals(Time other)
+        {
+            if (hours != other.Hours || minutes != other.Minutes || seconds != other.Seconds || miliseconds != other.Miliseconds)
+                return false;
+
+            return true;
+        }
+
+        public int CompareTo(Time other)
+        {
+            if (this.Equals(other))
+                return 0;
+            if (hours.Equals(other.Hours) == false)
+                return hours.CompareTo(other.Hours);
+            if (minutes.Equals(other.Minutes) == false)
+                return minutes.CompareTo(other.Minutes);
+            if (seconds.Equals(other.Seconds) == false)
+                return seconds.CompareTo(other.Seconds);
+
+            return miliseconds.CompareTo(other.Miliseconds);
+        }
+
+
+        public static bool operator ==(Time x, Time y) => x.Equals(y);
+        public static bool operator !=(Time x, Time y) => x.Equals(y) == false;
+        public static bool operator >(Time x, Time y) => x.CompareTo(y) == 1;
+        public static bool operator <(Time x, Time y) => x.CompareTo(y) == -1;
+        public static bool operator >=(Time x, Time y) => x.CompareTo(y) >= 0;
+        public static bool operator <=(Time x, Time y) => x.CompareTo(y) <= 0;
+        public Time Plus(TimePeriod other)
+        {
+            ushort mil = (ushort)(miliseconds + other.Miliseconds);
+            long sec = seconds + other.Seconds + mil / 1000;
+            mil %= 1000;
+            long min = minutes + sec / 60;
+            sec %= 60;
+            long hou = hours + min / 60;
+            min %= 60;
+            hou %= 24;
+
+            return new Time((byte)hou, (byte)min, (byte)sec, mil);
+        }
+
+
+        public static Time Plus(Time x, TimePeriod y) => x.Plus(y);
+        public static Time operator +(Time x, TimePeriod y) => x.Plus(y);
+
+
+    }
+}
---
+++ TimeOverride/TimeOverride/TimePeriod.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeOverride
+{
+    struct TimePeriod : IEquatable<TimePeriod>, IComparable<TimePeriod>
+    {
+        private readonly long seconds;
+        private readonly ushort miliseconds;
+
+        public long Seconds => seconds;
+        public ushort Miliseconds => miliseconds;
+
+        public TimePeriod(long hours, byte minutes, byte seconds = 0, ushort miliseconds = 0)
+        {
+            this.seconds = hours * 60 * 60 + (long)minutes * 60 + (long)seconds;
+            this.miliseconds = miliseconds;
+
+
+            if (hours < 0 || minutes < 0 || seconds < 0 || miliseconds < 0)
+                throw new ArgumentOutOfRangeException();
+
+            if (minutes >= 60 || seconds >= 60 || miliseconds >= 1000)
+                throw new ArgumentOutOfRangeException();
+        }
+
+        public TimePeriod(long seconds, ushort miliseconds = 0)
+        {
+            if (seconds < 0 || miliseconds < 0)
+                throw new ArgumentOutOfRangeException();
+
+            this.seconds = seconds;
+            this.miliseconds = miliseconds;
+        }
+
+
+
+        public TimePeriod(string s)
+        {
+            seconds = 0;
+            miliseconds = 0;
+            long hou;
+            byte min, sec; ;
+
+            if (s.Length == 10 || s.Length == 14)
+                throw new ArgumentException();
+
+            if (Int64.TryParse(s.Substring(0, 4), out hou) == false)
+                throw new ArgumentException();
+            if (hou < 0)
+                throw new ArgumentOutOfRangeException();
+
+            if (Byte.TryParse(s.Substring(5, 7), out min) == false)
+                throw new ArgumentException();
+            if (min >= 60 || min < 0)
+                throw new ArgumentOutOfRangeException();
+
+            if (Byte.TryParse(s.Substring(8, 10), out sec) == false)
+                throw new ArgumentException();
+            if (sec >= 60 || sec < 0)
+                throw new ArgumentOutOfRangeException();
+
+            if (s.Length == 14 && UInt16.TryParse(s.Substring(11, 14), out miliseconds) == false)
+                throw new ArgumentException();
+            if (miliseconds < 0)
+                throw new ArgumentOutOfRangeException();
+
+            seconds = (long)sec + (long)min * 60 + (long)hou * 60 * 60;
+        }
+
+
+
+        public TimePeriod(Time x1, Time x2)
+        {
+            seconds = ((long)x2.Hours - (long)x1.Hours) * 60 * 60 + ((long)x2.Minutes - (long)x1.Minutes) * 60 +
+                      ((long)x2.Seconds - (long)x1.Seconds);
+            if (x1.Miliseconds > x2.Miliseconds)
+            {
+                miliseconds = (ushort)(1000 + x2.Miliseconds - x1.Miliseconds);
+                seconds--;
+            }
+            else
+                miliseconds = (ushort)(x2.Miliseconds - x1.Miliseconds);
+        }
+
+
+        public override string ToString()
+        {
+            long sec = seconds % 60;
+            long min = (seconds / 60) % 60;
+            long hou = seconds / (60 * 60);
+
+            return $"{hou / 100}{(hou / 10) % 10}{hou % 10}:{min / 10}{min % 10}:{sec / 10}{sec % 10}";
+        }
+
+        public bool Equals(TimePeriod other)
+        {
+            if (seconds != other.Seconds || miliseconds != other.Miliseconds)
+                return false;
+
+            return true;
+        }
+
+        public int CompareTo(TimePeriod other)
+        {
+            if (this.Equals(other))
+                return 0;
+            if (seconds.Equals(other.Seconds) == false)
+                return seconds.CompareTo(other.Seconds);
+
+            return miliseconds.CompareTo(other.Miliseconds);
+        }
+
+
+        public static bool operator ==(TimePeriod x, TimePeriod y) => x.Equals(y);
+        public static bool operator !=(TimePeriod x, TimePeriod y) => x.Equals(y) == false;
+        public static bool operator >(TimePeriod x, TimePeriod y) => x.CompareTo(y) == 1;
+        public static bool operator <(TimePeriod x, TimePeriod y) => x.CompareTo(y) == -1;
+        public static bool operator >=(TimePeriod x, TimePeriod y) => x.CompareTo(y) >= 0;
+        public static bool operator <=(TimePeriod x, TimePeriod y) => x.CompareTo(y) <= 0;
+
+        public TimePeriod Plus(TimePeriod other) => new TimePeriod(seconds + other.Seconds + (miliseconds + other.Miliseconds) / 1000,
+                                                    (ushort)((miliseconds + other.Miliseconds) % 1000));
+        public static TimePeriod Plus(TimePeriod x, TimePeriod y) => x.Plus(y);
+        public static TimePeriod operator +(TimePeriod x, TimePeriod y) => x.Plus(y);
+
+    }
+}
